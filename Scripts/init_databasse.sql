f06b7fc0777007facfd5679bfd22320bd93e614a
Ali258-elsharkawy <83187254+Ali258-elsharkawy@users.noreply.github.com>
2025-03-04 00:47:49 +0200

    Update init_databasse.sql

--- Scripts/init_databasse.sql
+++ Scripts/init_databasse.sql
@@ -2,7 +2,6 @@ USE master
 GO
 
 -- Drop and recreate database warehouse
-
 IF EXISTS (SELECT 1 FROM SYS.databases WHERE name ='Datawarehouse')
 BEGIN
 	ALTER DATABASE datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
@@ -10,20 +9,19 @@ BEGIN
 END;
 GO
 
+-- creating the main database
 CREATE DATABASE Datawarehouse;
 GO
-
+-- Creating schemas for each layer
+	
 use Datawarehouse;
 GO
 
 create schema bornze;
-
 GO
 
 create schema silver;
-
 GO
 
 create schema gold;
-
 GO
